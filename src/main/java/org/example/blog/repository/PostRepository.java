71111db292ee73136b328a1b9ed0a62518daf21a
maciejjanowski <maciej.janowski7@hotmail.com>
2025-02-23 21:45:12 +0100

    Servie and Repository

--- src/main/java/org/example/blog/repository/PostRepository.java
+++ src/main/java/org/example/blog/repository/PostRepository.java
@@ -1,6 +1,9 @@
 package org.example.blog.repository;
 
+import org.example.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<org.example.blog.model.Post, Long> {
+
+ Post getPostById(Long id);
 }
--- src/main/java/org/example/blog/service/PostService.java
+++ src/main/java/org/example/blog/service/PostService.java
@@ -1,4 +1,42 @@
 package org.example.blog.service;
 
+import org.example.blog.model.Post;
+import org.example.blog.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+
+    private final PostRepository postRepository;
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+    public Post savePost(Post post) {
+        return postRepository.save(post);
+    }
+    public void deletePostById(Long id) {
+        postRepository.deleteById(id);
+    }
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+    public Post updatePost(Long id, Post updatedPost) {
+        Post post = postRepository.getPostById(id);
+        if (post != null) {
+            post.setTitle(updatedPost.getTitle());
+            post.setContent(updatedPost.getContent());
+
+        }
+
+        return postRepository.save(post);
+    }
 }
